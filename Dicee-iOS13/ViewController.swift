1b5d75388a2522d878b6528fb6474ef566b711ac
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-02 14:36:01 +0800

    Randomly generate a dice roll

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let DICE_OPTIONS = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
     }
 
-
+    @IBAction func roll(_ sender: UIButton) {
+        diceImageView1.image = DICE_OPTIONS[Int.random(in: 1...5)]
+        diceImageView2.image = DICE_OPTIONS[Int.random(in: 1...5)]
+    }
 }
 
